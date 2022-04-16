b4d427de4213406d3cb848bc976795f733882773
RamonaS2 <bandeiraramona2@gmail.com>
2022-04-16 13:35:44 -0300

    requisito 8

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,7 +1,11 @@
-const fetchProducts = async () => {
- const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+const fetchProducts = async (produto) => {
+  try {
+ const url = `https://api.mercadolibre.com/sites/MLB/search?q=${produto}`;
  const requisicao = await (await fetch(url)).json();
   return requisicao;
+} catch (error) {
+  return error;
+}
 };
 
 if (typeof module !== 'undefined') {
